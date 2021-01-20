103e598adab2117f7f910a3d51ef689da9960b14
Matthew Easley <matteasley8791@gmail.com>
2021-01-20 16:20:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,6 @@ def find_min_in_nested_arrays(src)
   row_index = 0 
   while row_index < src.count do 
 element_index = 0 
-new_array = []
 smallest_number = 100 
 while element_index < src[row_index].count do 
   if src[row_index][element_index] < smallest_number
